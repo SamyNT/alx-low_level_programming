840f940f47e03c64ede08cc5cab5e59036ad1e49
Samuel NT <samuelnkwindjat@gmail.com>
2022-09-13 02:45:58 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-/*
- * This program prints text
+/**
+ * main-Entry
+ *   * 
+ *    *This program prints text
+ *     *Return always zero
  */
-int main(
+int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
