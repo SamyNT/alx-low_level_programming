d1c9f5e7f15a0ee614d57ca0bac9158b1dc5dd65
Samuel NT <samuelnkwindjat@gmail.com>
2022-09-14 01:57:42 +0000

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *_islower - Function check if the input is a lower case letter
- *@c - number to compare
+ *@c: - number to compare
  * Return: 1 if true 0 if false
  */
 int _islower(int c)
