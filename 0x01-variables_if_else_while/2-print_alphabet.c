520e6ff896f90b65e8c82ecd94ed12e838bf9977
Samuel NT <samuelnkwindjat@gmail.com>
2022-09-24 08:08:59 +0000

    second commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the function prints the alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97 ; n <= 122 ; n++)
+		putchar(n);
+	putchar ('\n');
+	return (0);
+}
+
