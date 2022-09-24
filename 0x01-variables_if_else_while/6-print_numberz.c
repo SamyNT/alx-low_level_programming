e80ad2f13d55c86c25bd9cc993c438d4b8c50304
Samuel NT <samuelnkwindjat@gmail.com>
2022-09-24 08:39:19 +0000

    print the numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the function prints the alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+		putchar(n);
+	putchar ('\n');
+	return (0);
+}
+
