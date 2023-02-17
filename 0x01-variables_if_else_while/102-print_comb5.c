e380cb8d4aa570bfb8a2b3ffe4f82c217a4f5310
Samuel NT <gpe224@yahoo.com>
2023-02-17 21:04:55 +0000

    advanced task 3 third  trial

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main -  print all possible combinations of 2 digit numbers
- *
+ * I want the numbers to be printed as ij kl
  * Return: Always zero (FOR succes)
  */
 
@@ -10,26 +10,34 @@ int main(void)
 	int i, j, k, l;
 
 	i = 48;
-	j = 48;
 	while (i <= 57)
 	{
+		j = 48;
 		while (j <= 57)
 		{
-			k = i;
+			k = 48;
 			while (k <= 57)
 			{
-				l = j;
+				l = 48;
 				while (l <= 57)
 				{
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(k);
-					putchar(l);
-					if (i != 57 || j != 56 || k != 57 || l != 57)
+					int ij10 = (i - 48) * 10 + (j - 48);
+
+					int kl10 = (k - 48) * 10 + (l - 48);
+
+					if (kl10 > ij10)
 					{
-						putchar(',');
+						putchar(i);
+						putchar(j);
 						putchar(' ');
+						putchar(k);
+						putchar(l);
+
+						if (i != 57 || j != 56 || k != 57 || l != 57)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 					l++;
 				}
