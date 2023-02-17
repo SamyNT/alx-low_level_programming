40f34e396acb0088a7897b397b08e2bff24acfaa
Samuel NT <gpe224@yahoo.com>
2023-02-17 18:54:22 +0000

    advanced task 3 first trial

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+/**
+ * main -  print all possible combinations of 2 digit numbers
+ *
+ * Return: Always zero (FOR succes)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	i = 48;
+	j = 48;
+	while (i <= 57)
+	{
+		while (j <= 57)
+		{
+			k = i;
+			while (k <=57)
+			{
+				l = j + 1;
+				while (l <= 57)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i != 57 || j != 56 || k != 57 || l != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
