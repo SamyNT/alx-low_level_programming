1f5734451783c438790198bfa15f2d3b8322e327
Samuel NT <gpe224@yahoo.com>
2023-02-23 17:58:52 +0000

    commiting task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isdigit - Function check if the input is a  letter
+ *@c: - number to compare
+ * Return: 1 if true 0 if false
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+
