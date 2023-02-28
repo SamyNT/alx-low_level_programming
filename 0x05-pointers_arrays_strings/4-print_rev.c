df9c293f9028d146ef39181ac49dce88153fab75
Samuel NT <gpe224@yahoo.com>
2023-02-28 01:32:22 +0000

    updating task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,25 +9,17 @@
  */
 void print_rev(char *s)
 {
-	int count, i;
-	char temp;
+	int count;
 
 	count = 0;
 	while (*(s + count) != '\0')
 	{
 		count++;
 	}
-	for (i = 1; i < count/2; i++)
+	count--;
+	while (count  >= 0)
 	{
-		temp = *(s + i);
-		*(s + i) = *(s + count - 1 - i);
-		*(s + count - 1 - i) = temp;
-	}
-
-	while (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
+		_putchar(*(s + count--));
 	}
 	_putchar('\n');
 }
