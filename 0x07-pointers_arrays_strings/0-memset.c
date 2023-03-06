546065d8d633228ffc3d0792c3fa9d71ce84e0b1
Samuel NT <gpe224@yahoo.com>
2023-03-06 12:15:43 +0000

    initial commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - main function
+ * Description: fills the first n adresses of a string with a
+ * constant character c
+ *
+ * @s: string to be changed
+ * @b: constant character used to fill
+ * @n: number of bytes to filled by c
+ *
+ * Return: A pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (i = 0; i <= n - 1; i++)
+		s[i] = b;
+	return (s);
+}
