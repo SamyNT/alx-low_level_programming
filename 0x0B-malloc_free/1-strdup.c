8cd5af585a382a7da4aad963f1eb0d8392093641
Samuel NT <gpe224@yahoo.com>
2023-03-13 20:49:09 +0000

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - main function
+ * Description:  function that creates an array
+ * of chars, and initializes it with a specific char.
+ *
+ * @str: character used to initialize
+ * Return: pointer to array if succes else NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str = NULL;
+
+	new_str = (char *) malloc((strlen(str) + 1) * sizeof(char));
+	if (new_str != NULL && str != NULL)
+	{
+		strcpy(new_str, str);
+		return (new_str);
+	}
+	else
+		return (NULL);
+
+}
