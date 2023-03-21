93557d76880a941f46cb64a09b3ce63e8b6da3f9
Samuel NT <gpe224@yahoo.com>
2023-03-21 02:31:43 +0000

    fixing 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,48 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * new_dog - initializes a dog structure
- * @name: name
- * @age: age of dog
- * @owner: owner of dog
- *
- * Return: new dog
+ * new_dog - new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	int i = 0, j = 0, k;
+	dog_t *doge;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	doge = malloc(sizeof(dog_t));
+	if (doge == NULL)
+	{
+		free(doge);
 		return (NULL);
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
-	return (dog);
+	}
+	doge->name = malloc(i * sizeof(doge->name));
+	if (doge->name == NULL)
+	{
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		doge->name[k] = name[k];
+	doge->age = age;
+	doge->owner = malloc(j * sizeof(doge->owner));
+	if (doge->owner == NULL)
+	{
+		free(doge->owner);
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		doge->owner[k] = owner[k];
+	return (doge);
 }
