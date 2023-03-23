509d3bf6de9a72bda0c39ea53ad24c7911fc7142
Samuel NT <gpe224@yahoo.com>
2023-03-23 02:53:28 +0000

    task 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - iterates f through the size first members of an array
+ *
+ * @array: array to iterate through
+ * @size: number of elements in array
+ * @action: action to iterate
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	for ( ; i < size; i++)
+		action(array[i]);
+}
